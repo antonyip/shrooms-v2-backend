66773d4ad1a19a9c602230e8119e24cb61c8905c
Anton Yip <anton_yip@hotmail.com>
2023-09-16 07:21:32 +0800

    .env sample updates

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = 3000
+const port = process.env.PORT | 3000
 const { defaultEndpoint } = require('./defaultEndpoint')
 const { sendQueryWeb } = require('./sendQuery')
 const { getTokenWeb } = require('./getToken')
